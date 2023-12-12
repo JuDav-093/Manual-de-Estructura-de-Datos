e628a4a0c57f907c58161932c634b0b13c02424e
Cesar Loor <ciloor2@espe.edu.ec>
2023-12-12 18:19:02 -0500

    Ejemplo SobrecargaOperador

--- Sobrecarga/Ejemplo1.cpp
+++ Sobrecarga/Ejemplo1.cpp
@@ -1,12 +1,61 @@
 /*Ejemplo de sobrecarga de operadores utilizando los operadores + y % */
-#include<iostream>
+#include <iostream>
 
-using namespace std;
+class Numero {
+private:
+    int valor;
 
-main(){
-	
-	
-	
-	
-	
+public:
+    Numero() : valor(0) {}
+
+    Numero(int val) : valor(val) {}
+
+    // Sobrecarga del operador de suma (+)
+    Numero operator+(const Numero& otro) const {
+        Numero resultado;
+        resultado.valor = this->valor + otro.valor;
+        return resultado;
+    }
+
+    // Sobrecarga del operador de modulo (%)
+    Numero operator%(const Numero& otro) const {
+        Numero resultado;
+        resultado.valor = this->valor % otro.valor;
+        return resultado;
+    }
+
+    // Función para ingresar el valor del número desde el teclado
+    void ingresarNumero() {
+        std::cout << "Ingrese un numero: ";
+        std::cin >> valor;
+    }
+
+    // Función para imprimir el valor del número
+    void mostrarNumero() const {
+        std::cout << "Valor del numero: " << valor << std::endl;
+    }
+};
+
+int main() {
+    // Crear instancias de la clase Numero
+    Numero numero1, numero2;
+
+    // Ingresar valores desde el teclado
+    std::cout << "Para el primer numero:" << std::endl;
+    numero1.ingresarNumero();
+
+    std::cout << "\nPara el segundo numero:" << std::endl;
+    numero2.ingresarNumero();
+
+    // Sobrecarga del operador de suma
+    Numero resultadoSuma = numero1 + numero2;
+    std::cout << "\nResultado de la suma:" << std::endl;
+    resultadoSuma.mostrarNumero();
+
+    // Sobrecarga del operador de modulo
+    Numero resultadoModulo = numero1 % numero2;
+    std::cout << "\nResultado del modulo:" << std::endl;
+    resultadoModulo.mostrarNumero();
+
+    return 0;
 }
