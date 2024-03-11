119212abb19761d1485299ae9d757cefb84a7df6
Casignia Ruiz Diego Alejandro <132868844+DiegoCasignia@users.noreply.github.com>
2024-03-11 09:20:09 -0500

    Update main.cpp

--- P3/T2-Tecnicas de Analisis/4. Aritmetica de la Notacion O/Codigo/main.cpp
+++ P3/T2-Tecnicas de Analisis/4. Aritmetica de la Notacion O/Codigo/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include "Validacion.h"
 
-// FunciÛn para encontrar el m·ximo elemento en una matriz
+// Funci√≥n para encontrar el m√°ximo elemento en una matriz
 int encontrarMaximo(const std::vector<int>& arr) {
     int maxElemento = arr.at(0);
     for (int i = 1; i < arr.size(); ++i) {
@@ -13,8 +14,33 @@ int encontrarMaximo(const std::vector<int>& arr) {
 }
 
 int main() {
-    std::vector<int> numeros = { 10, 5, 8, 15, 3, 20, 12 };
-    int maximoElemento = encontrarMaximo(numeros);
-    std::cout << "El m·ximo elemento es: " << maximoElemento << std::endl;
-    return 0;
+    std::vector<int> numeros;
+    int maximoElemento;
+    do {
+        system("cls");
+        std::cout << "Aritmetica de la notacion O";
+        std::cout << "\n1. Agregar un numero";
+        std::cout << "\n2. Encontrar el numero maximo";
+        std::cout << "\n3. Salir";
+        int opcion = ingresoNumeroEntero((char*) "\nIngrese una opcion:  ");
+        switch (opcion) {
+        case 1:
+            numeros.push_back(ingresoNumeroEntero((char*)"\nIngrese un numero:  "));
+            break;
+        case 2:
+            if(numeros.size() != 0){
+                maximoElemento = encontrarMaximo(numeros);
+                std::cout << "\nEl maximo numero es: " << maximoElemento << std::endl;
+            }
+            else {
+                std::cout << "\nNo hay numeros\n";
+            }
+            system("pause");
+            break;
+        case 3:
+            return 0;
+        default:
+            break;
+        }
+    } while (true);
 }
