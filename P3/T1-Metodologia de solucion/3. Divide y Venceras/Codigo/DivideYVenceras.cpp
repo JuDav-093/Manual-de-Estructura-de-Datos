58899bda36b50b157b34f8bcfca0b0167a45dd01
Casignia Ruiz Diego Alejandro <132868844+DiegoCasignia@users.noreply.github.com>
2024-03-11 09:08:09 -0500

    Update DivideYVenceras.cpp

--- P3/T1-Metodologia de solucion/3. Divide y Venceras/Codigo/DivideYVenceras.cpp
+++ P3/T1-Metodologia de solucion/3. Divide y Venceras/Codigo/DivideYVenceras.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "Validacion.h"
 
 // Función para encontrar el máximo elemento en una matriz
 int encontrarMaximo(const std::vector<int>& arr, int izquierda, int derecha) {
@@ -15,8 +16,32 @@ int encontrarMaximo(const std::vector<int>& arr, int izquierda, int derecha) {
 }
 
 int main() {
-    std::vector<int> numeros = { 10, 5, 8, 15, 3, 20, 12 };
-    int maximoElemento = encontrarMaximo(numeros, 0, numeros.size() - 1);
-    std::cout << "El maximo elemento es: " << maximoElemento << std::endl;
-    return 0;
+    std::vector<int> numeros;
+    int maximoElemento;
+    do {
+        system("cls");
+        std::cout << "\n1. Agregar un numero";
+        std::cout << "\n2. Encontrar el numero maximo";
+        std::cout << "\n3. Salir";
+        int opcion = ingresoNumeroEntero((char*) "\nIngrese una opcion:  ");
+        switch (opcion) {
+        case 1:
+            numeros.push_back(ingresoNumeroEntero((char*)"\nIngrese un numero:  "));
+            break;
+        case 2:
+            if(numeros.size() != 0){
+                maximoElemento = encontrarMaximo(numeros, 0, numeros.size() - 1);
+                std::cout << "\nEl maximo numero es: " << maximoElemento << std::endl;
+            }
+            else {
+                std::cout << "\nNo hay numeros\n";
+            }
+            system("pause");
+            break;
+        case 3:
+            return 0;
+        default:
+            break;
+        }
+    } while (true);
 }
